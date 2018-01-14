2b05793ded3cf3f7bc65fa0f8ad6fdb4b2cc5f08
Joseph Birish <joe.birish@gmail.com>
2018-01-14 18:21:54 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -40,3 +40,10 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1)
 }
+
+// CHALLENGE: remove an element from the middle of the array/at a given index 
+// and return a COPY of the original array, minus that element
+
+function nonDestructivelyRemoveAtIndex(array, index) {
+// using slice _and_ spread??
+}
